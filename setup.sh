6891e8fd85a27255561a0a630fe2b5d2dfd6f3a7
LH <113438937+AdmiraLahav@users.noreply.github.com>
2026-04-25 18:52:50 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,8 +4,20 @@ apt update -y
 clear
 echo "[\x1b[38;5;12mINF\x1b[0m] Update done"
 apt upgrade -y 
+clear
+echo "[\x1b[38;5;12mINF\x1b[0m] Upgrade done"
 apt install python3 -y
+clear
+echo "[\x1b[38;5;12mINF\x1b[0m] Install python done"
 apt install hydra -y
+clear
+echo "[\x1b[38;5;12mINF\x1b[0m] Install hydra done"
 apt install nmap -y
+clear
+echo "[\x1b[38;5;12mINF\x1b[0m] Install nmap done"
 apt install nginx -y
+clear
+echo "[\x1b[38;5;12mINF\x1b[0m] Install nginx done"
 sudo systemctl disable nginx
+clear
+echo "[\x1b[38;5;12mINF\x1b[0m] Disable nginx done"
